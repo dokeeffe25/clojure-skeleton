e82aad30e97747718091525c403b7ce407106902
David O'Keeffe <david.okeeffe25@gmail.com>
2016-09-12 11:22:28 +0100

    Updated version to 0.1.0 (prevents error in leiningen)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-skeleton/lein-template "0.1.0-SNAPSHOT"
+(defproject clojure-skeleton/lein-template "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
